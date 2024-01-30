1441915c771e0142511067bc03b7876faef5727f
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-30 15:27:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function stringChop(str, size) {
-  // your code here
+  if (!str) {
+    return [];
+  }
+  
+  let len=size;
+  if(len<=0){
+    return [];
+  }
+
+  let result=[];
+
+  for(let i=0;i<str.length;i=i+len){
+    let sub=str.substring(i,i+len);
+    result.push(sub);
+  }
+
+  return result;
 }
 
 // Do not change the code below
